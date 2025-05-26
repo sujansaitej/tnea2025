c8952b86788cd028d704622ecb4442ac0b307b8c
Shaik120 <shaikmohammedali813@gmail.com>
2025-05-26 23:13:01 +0530

    updated

--- sample.py
+++ sample.py
@@ -1,17 +1,7 @@
 import gspread
-from google.oauth2.service_account import Credentials
 
-SCOPES = [
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
-]
-
-creds = Credentials.from_service_account_file('credentials.json', scopes=SCOPES)
-
-gc = gspread.authorize(creds)
+gc         = gspread.service_account("credentials.json")  # <- no scopes needed
 spreadsheet = gc.open_by_key("1x37iFC4Qg1l-voMcrxT5v71TxT9EXEoYIGIRy7RFU_k")
-worksheet = spreadsheet.worksheet("2025_data")
-
+worksheet   = spreadsheet.worksheet("2025_data")
 worksheet.append_row(["Test", "1234567890", "CSE", "OC", 199.0])
-
-print("✅ Successfully wrote to the sheet.")
+print("✅  Row written!")
